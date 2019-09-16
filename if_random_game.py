7f55b1c195855b223d14930b2172282d9f1f531c
Anna Chekareva <achekareva@mail.ru>
2019-09-16 18:24:57 +0300

    init commit

---
+++ if_random_game.py
@@ -0,0 +1,9 @@
+import random
+a=random.randint(1,4)
+b=int(input(("Введите число от 1 до 4: "))
+if a == b :
+    print("Победа")
+elif a>b:
+    print("Результат введенного числа больше")
+elif a<b:
+    print("Результат введенного числа меньше")
