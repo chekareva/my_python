59c70660a2f4c47006462c702f3fb5aa02ad0505
Anna Chekareva <achekareva@mail.ru>
2019-09-23 23:16:44 +0300

    init commit

---
+++ str_n.py
@@ -0,0 +1,15 @@
+def a(s,n):
+    if len(s)>n:
+        return (s.upper())
+    else:
+        return s
+s=input("Введите строку: ")
+n=int(input("Введите значение n: ")
+if s:
+      if n:
+          print (a(s,n))
+      else:
+          print("Введите число!")
+else:
+    ("Введите строку!")
+    
